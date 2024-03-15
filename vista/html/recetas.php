41e20c14eef8c80c10f0d03356a54e59d320addd
Andres <andres050903pipe@gmail.com>
2024-03-15 08:16:55 -0500

    controladores terminados facturas

--- vista/html/recetas.php
+++ vista/html/recetas.php
@@ -139,6 +139,9 @@
                 <div>
                     <select name="NombreInsumo[]" class="input">
                         <?php
+                            include '../../modelo/conexion.php';
+                            $sql = "SELECT * FROM tblinsumos";
+                            $result = $conexion->query($sql);
                             if ($result->num_rows > 0) {
                                 while($row = $result->fetch_assoc()) {
                                     echo "<option value='" . $row["IdInsumo"] . "'>" . $row["NombreInsumo"] . "</option>";
